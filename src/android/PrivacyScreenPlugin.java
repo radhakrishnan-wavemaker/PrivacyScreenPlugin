5739fae84919a7425d05005bcd99900f7396fa80
Radhakrishnan Asokan <74190299+radhakrishnan-wavemaker@users.noreply.github.com>
2021-06-21 16:43:11 +0530

    Update PrivacyScreenPlugin.java

--- src/android/PrivacyScreenPlugin.java
+++ src/android/PrivacyScreenPlugin.java
@@ -7,20 +7,11 @@
 package org.devgeeks.privacyscreen;
 
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
 import android.app.Activity;
-import android.view.Window;
 import android.view.WindowManager;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
-import android.os.Bundle;
 
 /**
  * This class sets the FLAG_SECURE flag on the window to make the app
