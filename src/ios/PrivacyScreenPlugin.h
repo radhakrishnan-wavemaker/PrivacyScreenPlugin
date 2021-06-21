32122b3a9f7af183e72525a276ef417517948eb6
Radhakrishnan <radhakrishnan.asokan@wavemaker.com>
2021-06-21 15:42:52 +0530

    Updated iOS with a blur screen and removed legacy code.

--- src/ios/PrivacyScreenPlugin.h
+++ src/ios/PrivacyScreenPlugin.h
@@ -7,17 +7,6 @@
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPlugin.h>
 
-typedef struct {
-  BOOL iPhone;
-  BOOL iPad;
-  BOOL iPhone4;
-  BOOL iPhone5;
-  BOOL iPhone6;
-  BOOL iPhone6Plus;
-  BOOL retina;
-  
-} CDV_iOSDevice;
-
 @interface PrivacyScreenPlugin : CDVPlugin
 
 @end
